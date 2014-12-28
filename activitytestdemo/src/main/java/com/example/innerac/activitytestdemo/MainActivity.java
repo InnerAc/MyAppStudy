b1bd1276746dfce73c7b971ed3ecd4949409b94e
InnerAc <anjicun@live.com>
2014-12-28 19:56:55 +0800

    在ActivityTestDemo主类中添加了两个指向性的按钮分别用来测试Theme和生命周期

--- activitytestdemo/src/main/java/com/example/innerac/activitytestdemo/MainActivity.java
+++ activitytestdemo/src/main/java/com/example/innerac/activitytestdemo/MainActivity.java
@@ -1,20 +1,47 @@
 package com.example.innerac.activitytestdemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
 
+    private Button btn_theme,btn_life;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
     }
 
+    protected void init(){
+        btn_theme = (Button)findViewById(R.id.btn_theme);
+        btn_life = (Button)findViewById(R.id.btn_life);
 
+        btn_life.setOnClickListener(listen);
+        btn_theme.setOnClickListener(listen);
+    }
+
+    View.OnClickListener listen = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Button bt = (Button)v;
+            int vid = bt.getId();
+            Intent intent = null;
+            if(vid == btn_theme.getId()){
+                intent = new Intent(MainActivity.this,ThemeActivity.class);
+            }else{
+                intent = new Intent(MainActivity.this,LifeActivity.class);
+            }
+            startActivity(intent);
+        }
+    };
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
